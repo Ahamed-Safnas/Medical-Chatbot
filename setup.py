458d401ad8db62e54e10418c72d0d1d89b0b6678
safnasNED25 <safnas2042ahamed@gmail.com>
2025-09-06 22:32:33 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= "Medical_chatbot",
+    version="0.1.0",
+    description="A chatbot for medical inquiries",
+    author="Ahamed Safnas",
+    author_email="safnas2042ahamed@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
